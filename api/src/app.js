0477922f445e2bcd2cc93354a5b2df6f2b12b7af
Aramis09 <aramisjaime48@gmail.com>
2023-03-01 09:26:56 -0300

    abierto para todos los puertos

--- api/src/app.js
+++ api/src/app.js
@@ -21,7 +21,7 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
-
+//a
 server.use('/', routes);
 
 // Error catching endware.
