2b950bf4418936a8399e6155a555a964bac50272
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-03-01 17:11:51 -0500

    add front routes

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,11 +1,16 @@
 import { Home } from "./pages/Home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
