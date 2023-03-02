132f24099e46a83791fd814ce866e23b3418758c
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-03-01 23:41:23 -0500

    typing product

--- Client/src/components/Product/Product.tsx
+++ Client/src/components/Product/Product.tsx
@@ -1,9 +1,12 @@
-import { PRUEBA } from "../../prueba";
 import { Card } from "../Card/Card";
-import { Games } from "../../prueba-get";
+import { PRUEBA } from "../../prueba";
 import styles from "./Product.module.css";
 
-export const Product = ({ limit }) => {
+interface Props {
+  limit: number;
+}
+
+export const Product = ({ limit }: Props) => {
   const cardSlice = PRUEBA.slice(0, limit);
 
   return (
