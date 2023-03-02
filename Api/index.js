44456fa3944e4fc9f13bbd96cac7f9e63556831a
Aramis09 <aramisjaime48@gmail.com>
2023-03-02 02:49:32 -0300

    me habia olvidado de poner bien el puerto y de setear el true de reinicio de DB en true

--- Api/index.js
+++ Api/index.js
@@ -1,9 +1,9 @@
 const app = require("./src/app");
 const {sequelize} = require("./src/db");
 
-const PORT = process.env.PORT || 3011; //volver a 3001
+const PORT = process.env.PORT || 3001; //volver a 3001
 
 app.listen(PORT,()=>{
-    sequelize.sync({force:false}); //volver a true
+    sequelize.sync({force:true}); //volver a true
     console.log(`Listening on: http://localhost:${PORT}`);
 });
