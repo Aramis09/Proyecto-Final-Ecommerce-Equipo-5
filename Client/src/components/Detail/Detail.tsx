8ece86e3d3869f1fcf0b8b578914079d03dd042b
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-03-02 04:13:24 -0500

    Finish a detail component

--- Client/src/components/Detail/Detail.tsx
+++ Client/src/components/Detail/Detail.tsx
@@ -1,21 +1,10 @@
-import { useState } from "react";
 import { PRUEBA } from "../../prueba";
 import { Rating } from "../Rating/Rating";
 import styles from "./Detail.module.css";
+import { DetailCarousel } from "./DetailCarousel";
 
 export const Detail = () => {
   const game = PRUEBA.slice(0, 1);
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const nextSlide = () => {
-    setCurrentSlide((currentSlide + 1) % game[0].images.length);
-  };
-
-  const prevSlide = () => {
-    setCurrentSlide(
-      (currentSlide - 1 + game[0].images.length) % game[0].images.length
-    );
-  };
 
   return (
     <>
@@ -24,62 +13,43 @@ export const Detail = () => {
           <img key={index} src={item.background_image} alt={item.name} />
         ))}
       </section>
-      <section>
-        <div>
+      <section className={styles["info-container"]}>
+        <div className={styles["left-section"]}>
           {game.map((item) => (
             <div key={item.id}>
               <h3>{item.name}</h3>
               <p>${item.price}</p>
-              <p>{item.rating}</p>
               <Rating value={item.rating} />
             </div>
           ))}
         </div>
-        <div>
-          {game.map((item) => (
-            <div key={item.id}>
-              <p>{item.description}</p>
-              <p>{item.genres}</p>
-              <p>{item.platforms}</p>
+        <div className={styles["right-section"]}>
+          {game.map(({ id, description, genres, platforms }, index) => (
+            <div key={id}>
+              <p>{description}</p>
+              <div className={styles["right-section-info"]} key={index}>
+                <div className={styles["gender-section"]}>
+                  <h4>Generos</h4>
+                  <div className={styles["button-container"]}>
+                    {genres.map((item, index) => (
+                      <button key={index}>{item}</button>
+                    ))}
+                  </div>
+                </div>
+                <div className={styles["platforms-section"]} key={index}>
+                  <h4>Plataformas</h4>
+                  <div className={styles["button-container"]}>
+                    {platforms.map((item) => (
+                      <button>{item}</button>
+                    ))}
+                  </div>
+                </div>
+              </div>
             </div>
           ))}
         </div>
       </section>
-      <section className={styles.carousel}>
-        <div className={styles["carousel-container"]}>
-          {game[0].images.map((image, index) => (
-            <div
-              key={index}
-              className={
-                index === currentSlide
-                  ? styles["carousel-slide"] + " " + styles["active-slide"]
-                  : styles["carousel-slide"]
-              }
-            >
-              {<img src={image}  />}
-            </div>
-          ))}
-          <button className={styles["carousel-prev"]} onClick={prevSlide}>
-            &#10094;
-          </button>
-          <button className={styles["carousel-next"]} onClick={nextSlide}>
-            &#10095;
-          </button>
-        </div>
-        <div className={styles["carousel-indicators"]}>
-          {game[0].images.map((image, index) => (
-            <span
-              key={index}
-              className={
-                index === currentSlide
-                  ? styles["carousel-indicator"] + " " + styles["active-indicator"]
-                  : styles["carousel-indicator"]
-              }
-              onClick={() => setCurrentSlide(index)}
-            ></span>
-          ))}
-        </div>
-      </section>
+      <DetailCarousel />
     </>
   );
 };
