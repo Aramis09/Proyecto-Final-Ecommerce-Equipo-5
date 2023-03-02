6c610f0df363f94f26f9f3e657eed3d017975bca
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-03-02 03:19:14 -0500

    create a detailCarousel and add styles

---
+++ Client/src/components/Detail/DetailCarousel.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { PRUEBA } from "../../prueba";
+import styles from "./DetailCarousel.module.css";
+
+export const DetailCarousel = () => {
+  const game = PRUEBA.slice(0, 1);
+  const imageLinks = game[0].images;
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const handleChangeImageNext = () => {
+    setCurrentImage(
+      currentImage === imageLinks.length - 1 ? 0 : currentImage + 1
+    );
+  };
+
+  const handleChangeImagePrev = () => {
+    setCurrentImage(
+      currentImage === 0 ? imageLinks.length - 1 : currentImage - 1
+    );
+  };
+
+  return (
+    <section className={styles["carousel-container"]}>
+      <button onClick={handleChangeImagePrev}>←</button>
+      {imageLinks.map((image, index) => (
+        <div
+          key={index}
+          className={`${styles["carousel-img"]} ${
+            currentImage === index ? styles["active-img"] : ""
+          }`}
+        >
+          <img src={image} alt={`Game screenshot ${index + 1}`} />
+        </div>
+      ))}
+      <button onClick={handleChangeImageNext}>→</button>
+    </section>
+  );
+};
