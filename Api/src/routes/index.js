4bf32db3c0bf215ee96cce538fea4775c6f094b0
Aramis09 <aramisjaime48@gmail.com>
2023-03-03 16:28:09 -0300

    fixed

--- Api/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require("express");
-const productsRouter = require("./subRoutes/productsRouter");
-const mainRouter = Router();
-
-mainRouter.use("/products",productsRouter);
-
-
-module.exports = mainRouter;
