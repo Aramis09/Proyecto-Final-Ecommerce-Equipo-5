d5945ee63f7ffe1e0c5d0d3c13667e2c377a541f
Daniel Pinzón <102686918+DanSeph08@users.noreply.github.com>
2023-03-03 13:51:49 -0500

    Arreglo pf

--- Client/src/components/NavBar/NavBar.tsx
+++ Client/src/components/NavBar/NavBar.tsx
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import style from './NavBar.module.css';
-import icon from '../../assets/joystick_icon.png';
-
-
-
-export const NavBar = () => {
-  return (
-    <div className={style.mainContainer}>
-      <nav>
-        <ul className={style.listContainer}>
-          <li>
-            <img src={icon} alt="joystick_icon" />
-          </li>
-          <li>
-            SEARCHBAR
-          </li>
-          <li>
-            <button className={style.loginButton}>LOGIN</button>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
-};
-=======
 import { Link } from "react-router-dom";
 import style from "./NavBar.module.scss";
 import icon from "../../assets/joystick_icon.png";
@@ -47,4 +21,3 @@ export const NavBar = () => {
     </div>
   );
 };
->>>>>>> c7621a519d5c7c592ef8ae839718b8997b94e158
