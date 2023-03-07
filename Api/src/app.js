17f24f595d8f0fbabe9d89828ea70eff6314f339
Cruz Rocha <juancruzrocha@live.com>
2023-03-07 17:37:34 -0300

    Auth0 implemented in the back end
    Auth0 implementado en el back end
    En las rutas:
    localhost:3000/
    
    /login
    /logout
    /profile

--- Api/src/app.js
+++ Api/src/app.js
@@ -1,8 +1,36 @@
 const express = require("express");
 const morgan = require("morgan");
-const cookieParser = require('cookie-parser');//**esto siver para los CORS */
+const cookieParser = require("cookie-parser"); //**esto siver para los CORS */
 const mainRouter = require("./routes/index");
-const app = express();  //**Este es nuestro servidor */
+const app = express(); //**Este es nuestro servidor */
+const { auth } = require("express-openid-connect");
+
+// const {
+//   AUTH_REQUIRED,
+//   AUTH0LOGOUT,
+//   SECRET,
+//   BASE_URL,
+//   CLIENT_ID,
+//   ISSUER_BASE_URL,
+// } = process.env;
+
+// const config = {
+//   AUTH_REQUIRED,
+//   AUTH0LOGOUT,
+//   SECRET,
+//   BASE_URL,
+//   CLIENT_ID,
+//   ISSUER_BASE_URL,
+// };
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: 'http://localhost:3000',
+  clientID: 'K1WxZJeGyTg75TOD5gxBXVZZ6DG9EsyV',
+  issuerBaseURL: 'https://henryfinalproject.us.auth0.com',
+  secret: 'rBQlxo8TBg96u6W3dtE5SAAoH9io0-nK_7FEMRLbrxJvT8HrzkHjkJ1DGUN0qNkF'
+};
 
 //**.use : cuando recibas una request que pase por aca */
 //**Middlewares a usar: Son funciones que se encargan de hacer algo con la request y continua */
@@ -10,18 +38,24 @@ app.use(morgan("dev"));
 app.use(express.json()); //**traduce de json a js object */
 app.use(cookieParser());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-  });
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 //** EndPoints Peticiones */
+
+app.use(auth(config));
 
 app.use(mainRouter);
 
 // Error catching endware. Manejo de errores
-app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+app.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -2,8 +2,10 @@ const {Router} = require("express");
 const productsRouter = require("./subRoutes/productsRouter");
 const platformsRouter = require("./subRoutes/platformsRouter");
 const genresRouter = require("./subRoutes/genresRouter");
+const loginRouter = require("./subRoutes/loginRouter");
 const mainRouter = Router();
 
+mainRouter.use("/",loginRouter);
 mainRouter.use("/products",productsRouter);
 mainRouter.use("/platforms",platformsRouter);
 mainRouter.use("/genres",genresRouter);
