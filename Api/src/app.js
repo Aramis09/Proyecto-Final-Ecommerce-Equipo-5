3e823df0b51b1087e70c1cb5d2bd88d45a17a5ee
Cruz Rocha <juancruzrocha@live.com>
2023-03-08 11:12:18 -0300

    auth0 implemented in the front end
    auth0 implementado en el font end
    Borrado del auth0 en el back end

--- Api/src/app.js
+++ Api/src/app.js
@@ -3,34 +3,7 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser"); //**esto siver para los CORS */
 const mainRouter = require("./routes/index");
 const app = express(); //**Este es nuestro servidor */
-const { auth } = require("express-openid-connect");
 
-// const {
-//   AUTH_REQUIRED,
-//   AUTH0LOGOUT,
-//   SECRET,
-//   BASE_URL,
-//   CLIENT_ID,
-//   ISSUER_BASE_URL,
-// } = process.env;
-
-// const config = {
-//   AUTH_REQUIRED,
-//   AUTH0LOGOUT,
-//   SECRET,
-//   BASE_URL,
-//   CLIENT_ID,
-//   ISSUER_BASE_URL,
-// };
-
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  baseURL: 'http://localhost:3000',
-  clientID: 'K1WxZJeGyTg75TOD5gxBXVZZ6DG9EsyV',
-  issuerBaseURL: 'https://henryfinalproject.us.auth0.com',
-  secret: 'rBQlxo8TBg96u6W3dtE5SAAoH9io0-nK_7FEMRLbrxJvT8HrzkHjkJ1DGUN0qNkF'
-};
 
 //**.use : cuando recibas una request que pase por aca */
 //**Middlewares a usar: Son funciones que se encargan de hacer algo con la request y continua */
@@ -49,7 +22,7 @@ app.use((req, res, next) => {
 });
 //** EndPoints Peticiones */
 
-app.use(auth(config));
+
 
 app.use(mainRouter);
 
--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -2,10 +2,9 @@ const {Router} = require("express");
 const productsRouter = require("./subRoutes/productsRouter");
 const platformsRouter = require("./subRoutes/platformsRouter");
 const genresRouter = require("./subRoutes/genresRouter");
-const loginRouter = require("./subRoutes/loginRouter");
 const mainRouter = Router();
 
-mainRouter.use("/",loginRouter);
+
 mainRouter.use("/products",productsRouter);
 mainRouter.use("/platforms",platformsRouter);
 mainRouter.use("/genres",genresRouter);
