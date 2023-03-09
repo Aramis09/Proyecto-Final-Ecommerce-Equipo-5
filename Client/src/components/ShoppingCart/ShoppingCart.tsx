e1cdebc8b9a8acb28e918bdbdd85bd0f2d22d626
Daniel Pinzón <102686918+DanSeph08@users.noreply.github.com>
2023-03-09 12:10:17 -0500

    Preparation

--- Client/src/components/ShoppingCart/ShoppingCart.tsx
+++ Client/src/components/ShoppingCart/ShoppingCart.tsx
@@ -18,40 +18,50 @@ export const ShoppingCart = () => {
     };
 
     let listProductsShoppingCart: object[] = useAppSelector((state) => state.shoppingCartReducer.listProductsShoppingCart);
-    let totalAmount: number = useAppSelector((state) => state.shoppingCartReducer.totalAmount);
-
+	let totalAmount: number = useAppSelector((state) => state.shoppingCartReducer.totalAmount);
+	
     if(listProductsShoppingCart.length > 0){
         return (
-            <div>
-                <button
-                    className={style.cartButton}
-                    onClick={ev => handleOpenModal(ev)}>
-                    <img
-                        className={style.cartIcon}
-                        src={cart}
-                        alt="cart_icon" />
-                </button>
-                {modalOpen && <div className={style.modalContainer}>
-                    <div className={style.modalContent}>
-                        <button
-                            className={style.closeModalButton}
-                            onClick={ev => handleCloseModal(ev)}>X
-                        </button>
-    
-                        <table>
-                            <tbody>
-                                { listProductsShoppingCart.map( (item, index) =>  <tr key={index}><td>{item.name}</td><td>$/{item.price}</td></tr>) }
-                                <tr><td>MONTO A PAGAR: </td><td>$/{totalAmount}</td></tr>
-                            </tbody>
-                        </table>
-                        <Link to={'/checkout'}>
-                            <button>CHECKOUT</button>
-                        </Link>
-                    </div>
-                </div>
-                }
-            </div>
-        );
+					<div>
+						<button
+							className={style.cartButton}
+							onClick={(ev) => handleOpenModal(ev)}>
+							<img className={style.cartIcon} src={cart} alt='cart_icon' />
+						</button>
+						{modalOpen && (
+							<div className={style.modalContainer}>
+								<div className={style.modalContent}>
+									<button
+										className={style.closeModalButton}
+										onClick={(ev) => handleCloseModal(ev)}>
+										X
+									</button>
+
+									<table>
+										<tbody>
+											{listProductsShoppingCart.map((item, index) => (
+												<tr key={index}>
+													{/* <td><img src={item.background_image}></img></td> */}
+													<td>{item.name}</td>
+													<td>$/{item.price}</td>
+												</tr>
+											))}
+											<br />
+											<tr>
+												<td>MONTO A PAGAR: </td>
+												<td>$/{totalAmount}</td>
+											</tr>
+										</tbody>
+									</table>
+
+									<Link to={'/checkout'}>
+										<button>CHECKOUT</button>
+									</Link>
+								</div>
+							</div>
+						)}
+					</div>
+				);
     }else{
         return (
             <div>
