f5eea9f64fec1a1dc8d0f36559e0882f140fdd06
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-03-15 20:16:39 -0500

    verificated if user is a admin

--- Client/src/components/NavBar/NavBar.tsx
+++ Client/src/components/NavBar/NavBar.tsx
@@ -1,51 +1,53 @@
 import { Link } from "react-router-dom";
 import style from "./NavBar.module.scss";
 import { SearchBar } from "../SearchBar/SearchBar";
-import { ShoppingCart } from '../../components/ShoppingCart/ShoppingCart';
+import { ShoppingCart } from "../../components/ShoppingCart/ShoppingCart";
 import icon from "../../assets/joystick_icon.png";
 import { useAuth0 } from "@auth0/auth0-react";
 //import { Button } from "@mui/material";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import axios from "axios";
 
-
 export const NavBar = () => {
-
   const { loginWithRedirect, user, logout, isAuthenticated } = useAuth0();
 
   useEffect(() => {
     const addNewUser = async () => {
-      if(typeof user !== 'undefined'){
-        await axios.get(`http://localhost:3001/user/userNew?email=${user.email}`);
+      if (typeof user !== "undefined") {
+        await axios.get(
+          `http://localhost:3001/user/userNew?email=${user.email}`
+        );
       }
-    }
-    addNewUser()
-  })
+    };
+    addNewUser();
+  });
 
   return (
-		<nav className={style.mainContainer}>
-			<div className={style.listContainer}>
-				<Link to='/'>
-					<img className={style.home} src={icon} alt='joystick_icon' />
-				</Link>
-				<SearchBar />
-				<ShoppingCart />
-				{isAuthenticated ? (
-					<button
-						className={style.loginButton}
-						onClick={() =>
-							logout({ logoutParams: { returnTo: window.location.origin } })
-						}>
-						LOG OUT
-					</button>
-				) : (
-					<button
-						className={style.loginButton}
-						onClick={() => loginWithRedirect()}>
-						LOG IN
-					</button>
-				)}
-			</div>
-		</nav>
-	);
+    <nav className={style.mainContainer}>
+      <div className={style.listContainer}>
+        <Link to="/">
+          <img className={style.home} src={icon} alt="joystick_icon" />
+        </Link>
+        <SearchBar />
+        <ShoppingCart />
+        {isAuthenticated ? (
+          <button
+            className={style.loginButton}
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            LOG OUT
+          </button>
+        ) : (
+          <button
+            className={style.loginButton}
+            onClick={() => loginWithRedirect()}
+          >
+            LOG IN
+          </button>
+        )}
+      </div>
+    </nav>
+  );
 };
--- Client/src/pages/Home/Home.tsx
+++ Client/src/pages/Home/Home.tsx
@@ -44,6 +44,9 @@ export const Home = () => {
     <div className={styles.container}>
       <NavBar />
       <Carousel />
+      <Link to={'/users'}>
+      <button>Dashboard</button>
+      </Link>
       <h2 className={styles.title}>TOP GAMES</h2>
       <Product/>
       <Link to={'/products'}>
