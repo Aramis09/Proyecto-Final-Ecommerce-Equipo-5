30b81ec4acec2496268284380697c9cb9614461a
jjmartinez <juanjomm71@gmail.com>
2023-03-20 13:34:09 -0300

    working on Footer component

--- Client/src/pages/Home/Home.tsx
+++ Client/src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { Carousel } from "../../components/Carousel/Carousel";
 import { NavBar } from "../../components/NavBar/NavBar";
 import { Product } from "../../components/Product/Product";
+import { Footer } from "../../components/Footer/Footer";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks/hooks";
 import { getProductsByFilters } from "../../redux/actions/productAction";
 //import { getShoppingCartUserFromDB } from "../../redux/actions/shoppingCartAction";
@@ -13,23 +14,23 @@ export const Home = () => {
   const dispatch = useAppDispatch();
 
   const toTheSearchList = (e: any) => {
-    
+
     dispatch(getProductsByFilters({
-      name:'',
-			filters:
-				{
-				genres:[],
-				platform: [],
-				priceRange:[0,100]
-				},
-			order:
-			{
-				alphabetic:'',
-				price:''
-			}    
+      name: '',
+      filters:
+      {
+        genres: [],
+        platform: [],
+        priceRange: [0, 100]
+      },
+      order:
+      {
+        alphabetic: '',
+        price: ''
+      }
     }))
   }
-  
+
   return (
     <div className={styles.container}>
       <NavBar />
@@ -37,17 +38,18 @@ export const Home = () => {
       <Link to={'/friends'}>
         <button>Friends</button>
       </Link>
-      <Link to = "/wish">wish</Link><br />
-      <Link to = "/library">library</Link>
+      <Link to="/wish">wish</Link><br />
+      <Link to="/library">library</Link>
 
       <Link to={'/users'}>
-      <button>Dashboard</button>
+        <button>Dashboard</button>
       </Link>
       <h2 className={styles.title}>TOP GAMES</h2>
-      <Product/>
+      <Product />
       <Link to={'/products'}>
         <button className={styles['More-products-btn']} value='0' onClick={toTheSearchList}>More</button>
       </Link>
+      <Footer />
     </div>
   );
 };
